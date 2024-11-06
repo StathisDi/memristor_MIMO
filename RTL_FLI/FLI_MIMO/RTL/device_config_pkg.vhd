491b396ef94037573335aa4c4a735e79f1b316bd
stathis <stathis@kth.se>
2024-11-06 10:55:05 +0100

    updated the test with different devices

--- RTL_FLI/FLI_MIMO/RTL/device_config_pkg.vhd
+++ RTL_FLI/FLI_MIMO/RTL/device_config_pkg.vhd
@@ -5,9 +5,9 @@ USE IEEE.math_real.ALL;
 
 package device_config_pkg is
 
-constant device_type: string := "MF";
-constant device_states: integer := 150;
-constant dt: time := 0.0200000000 sec;
+constant device_type: string := "ferro";
+constant device_states: integer := 400;
+constant dt: time := 0.0000002000 sec;
 constant duty_cycle: real := 0.5;
 
 
